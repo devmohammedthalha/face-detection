6ea2c3ce3d800e341a5b57a21f92c63354d35858
devmohammedthalha <devmohammedthalha@gmail.com>
2025-03-16 13:45:03 +0530

    Scripts Updated

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ const run = async()=>{
     // pre-trained machine learning for our facial detection!
     await Promise.all([
         faceapi.nets.ssdMobilenetv1.loadFromUri('./models'),
-        faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+        faceapi.nets.faceLandmark68Net.loadFromUri('./models'), 
         faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
         faceapi.nets.ageGenderNet.loadFromUri('./models'),
         faceapi.nets.faceExpressionNet.loadFromUri('./models'),
@@ -23,23 +23,26 @@ const run = async()=>{
     const canvas = document.getElementById('canvas')
     canvas.style.left = videoFeedEl.offsetLeft
     canvas.style.top = videoFeedEl.offsetTop
-    canvas.height = videoFeedEl.height
+    canvas.height = videoFeedEl.height 
     canvas.width = videoFeedEl.width
 
     // Load multiple reference images
     const refImages = [
-        'https://media.licdn.com/dms/image/v2/D5603AQGA7P530nogyw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1695441541808?e=1736380800&v=beta&t=pXuxrcKDpeljeU-3CjSvyrwbfGIvA9MpWemKWFSXc84',
+        'https://media.licdn.com/dms/image/v2/D5603AQGA7P530nogyw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1695441541808?e=1742428800&v=beta&t=m0e6abzGp3Mzt5BJ3cwVszM4VZ-nFyr41oLPIiQvQ64',
         'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Michael_Jordan_in_2014.jpg/220px-Michael_Jordan_in_2014.jpg',
+        // "https://scontent.fcjb1-2.fna.fbcdn.net/v/t39.30808-6/464194022_8468681629896996_187184252050306204_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=waE4ZHu8LBIQ7kNvgHtNN4p&_nc_zt=23&_nc_ht=scontent.fcjb1-2.fna&_nc_gid=AYlU1TlVuogc3dnK1FAFLIk&oh=00_AYBsfxo2WrVcNGDsmC9JhWDy8VKa2kZ4hH6Gqu9uOepzlA&oe=673024F1",
+        // "https://media.licdn.com/dms/image/v2/D5603AQHtqbXuNpjizQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1730732892409?e=1736380800&v=beta&t=t_1BKrkPHbdGd0G3Aq-HMXLw__SHHNAI39NGpOIPlVI"
         // Add more URLs as needed
     ];
 
+    const refNames = ["Thalha", "Jordan"]
      // Fetch and process reference images
      const labeledDescriptors = await Promise.all(
         refImages.map(async (url, index) => {
             const img = await faceapi.fetchImage(url);
             const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
             if (!detections) return null;
-            return new faceapi.LabeledFaceDescriptors(`Person ${index + 1}`, [detections.descriptor]);
+            return new faceapi.LabeledFaceDescriptors(`${refNames[index]}`, [detections.descriptor]);
         })
     ).then(descriptors => descriptors.filter(desc => desc !== null));
 
